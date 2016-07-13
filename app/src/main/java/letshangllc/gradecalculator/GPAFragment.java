1e35f6cf3e28a311ec28ed14bb24e5dd6c0915a4
Carl Burnham <cburnham@letshang.us>
2016-07-13 14:01:41 -0400

    finished gpa calc

--- app/src/main/java/letshangllc/gradecalculator/GPAFragment.java
+++ app/src/main/java/letshangllc/gradecalculator/GPAFragment.java
@@ -98,42 +98,32 @@ public class GPAFragment extends Fragment {
 
     private double getGPA(String letterGrade) {
         switch (letterGrade) {
+            case "A+":
+                return 4.3;
             case "A":
                 return 4;
-
             case "A-":
                 return 3.7;
-
             case "B+":
                 return 3.3;
-
             case "B":
                 return 3.0;
-
             case "B-":
                 return 2.7;
-
             case "C+":
                 return 2.3;
-
             case "C":
                 return 2.0;
-
             case "C-":
                 return 1.7;
-
             case "D+":
                 return 1.3;
-
             case "D":
                 return 1.0;
-
             case "D-":
                 return 0.7;
-
             case "F":
                 return 0;
-
         }
         return 0;
 
--- app/src/main/java/letshangllc/gradecalculator/GradeCalculatorFragment.java
+++ app/src/main/java/letshangllc/gradecalculator/GradeCalculatorFragment.java
@@ -55,7 +55,7 @@ public class GradeCalculatorFragment extends Fragment {
         //setListeners();
 
         sections = 0;
-        for(int i = 0; i<10; i++){
+        for(int i = 0; i<7; i++){
             addGradeView();
         }
         return view;
